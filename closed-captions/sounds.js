e2e30f64446d85cf6c9731a8614b12370f3f3cdb
akshay197 <akshay.ak@hotmail.com>
2022-06-08 21:52:51 +0200

    Voortgang commit

--- closed-captions/sounds.js
+++ closed-captions/sounds.js
@@ -1,15 +1,20 @@
 // Hier kan je tijden toevoegen in secondes
-// Op die mommenten komt er een class op de body.
+// Op die momenten komt er een class op de body.
 sounds = [
 	0,
 	9,
 	12,
 	15,
-	16.5,
+	15.5,
 	24,
-	25.8,
+	25,
 	31,
 	32,
-	34,
-	86
+	33,
+	65,
+	70,
+	86,
+	101,
+	119.5,
+	130
 ];
