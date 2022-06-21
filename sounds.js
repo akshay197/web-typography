0c2c26309e2a278981dd7f4d03f387c8e2393929
akshay197 <akshay.ak@hotmail.com>
2022-06-21 22:55:17 +0200

    Minor changes

---
+++ sounds.js
@@ -0,0 +1,20 @@
+// Hier kan je tijden toevoegen in secondes
+// Op die momenten komt er een class op de body.
+sounds = [
+	0,
+	9,
+	12,
+	15,
+	15.5,
+	24,
+	25,
+	31,
+	32,
+	33,
+	65,
+	70,
+	86,
+	101,
+	119.5,
+	130
+];
